84caa3159deacf1cc6d66cb422e01bbef7a956c4
Tezcalipoca <tezcatlipoca0211@gmail.com>
2023-11-27 19:51:53 +0700

    first commit - init project

---
+++ src/main/java/com/ptithcm/pe/PersonalExpenditure.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ptithcm.pe;
+
+/**
+ *
+ * @author tezca
+ */
+public class PersonalExpenditure {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
